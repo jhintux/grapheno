9c24993917cdcc7033e03a704876bb14eb55cfd2
Eduardo <ed.l7@icloud.com>
2025-11-30 13:34:46 -0500

    [feat] implementation of types Blockchian, Block, BlockHeader, Transaction and basic functionality for these

---
+++ lib/src/sha256.rs
@@ -0,0 +1,38 @@
+use crate::U256;
+use serde::{Deserialize, Serialize};
+use sha256::digest;
+use std::fmt;
+
+#[derive(Clone, Copy, Serialize, Deserialize, Debug, PartialEq, Eq)]
+pub struct Hash(U256);
+
+impl Hash {
+    // hash anything that can be serde Serialized via ciborium
+    pub fn hash<T: serde::Serialize>(data: &T) -> Self {
+        let mut serialized: Vec<u8> = vec![];
+        if let Err(e) = ciborium::into_writer(data, &mut serialized) {
+            panic!("Failed to serialize data: {:?}. This should not happen", e);
+        }
+        let hash = digest(&serialized);
+        let hash_bytes = hex::decode(hash).unwrap();
+        let hash_array: [u8; 32] = hash_bytes.as_slice().try_into().unwrap();
+
+        Hash(U256::from_big_endian(&hash_array))
+    }
+
+    // check if a hash matches a target
+    pub fn matches_target(&self, target: U256) -> bool {
+        self.0 <= target
+    }
+
+    //zero hash
+    pub fn zero() -> Self {
+        Hash(U256::from(0))
+    }
+}
+
+impl fmt::Display for Hash {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:x}", self.0)
+    }
+}
---
+++ lib/src/types.rs
@@ -0,0 +1,107 @@
+use crate::{
+    U256,
+    crypto::{PublicKey, Signature},
+    sha256::Hash,
+    util::MerkleRoot,
+};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Blockchain {
+    pub blocks: Vec<Block>,
+}
+
+impl Blockchain {
+    pub fn new() -> Self {
+        Self { blocks: vec![] }
+    }
+
+    pub fn add_block(&mut self, block: Block) {
+        self.blocks.push(block);
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Block {
+    pub header: BlockHeader,
+    pub transactions: Vec<Transaction>,
+}
+
+impl Block {
+    pub fn new(header: BlockHeader, transactions: Vec<Transaction>) -> Self {
+        Block {
+            header: header,
+            transactions: transactions,
+        }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct BlockHeader {
+    pub timestamp: DateTime<Utc>,
+    pub nonce: u64,
+    pub prev_block_hash: Hash,
+    pub merkle_root: MerkleRoot,
+    /// A number, which has to be higher than the hash of this block for it to be considered valid.
+    pub target: U256,
+}
+
+impl BlockHeader {
+    pub fn new(
+        timestamp: DateTime<Utc>,
+        nonce: u64,
+        prev_block_hash: Hash,
+        merkle_root: MerkleRoot,
+        target: U256,
+    ) -> Self {
+        BlockHeader {
+            timestamp,
+            nonce,
+            prev_block_hash,
+            merkle_root,
+            target,
+        }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct Transaction {
+    pub inputs: Vec<TransactionInput>,
+    pub outputs: Vec<TransactionOutput>,
+}
+
+impl Transaction {
+    pub fn new(inputs: Vec<TransactionInput>, outputs: Vec<TransactionOutput>) -> Self {
+        Transaction { inputs, outputs }
+    }
+    pub fn hash(&self) -> ! {
+        unimplemented!()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionInput {
+    pub prev_transaction_output_hash: Hash,
+    pub signature: Signature,
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+pub struct TransactionOutput {
+    pub value: u64,
+    pub unique_id: Uuid,
+    pub pubkey: PublicKey,
+}
+
+impl TransactionOutput {
+    pub fn hash(&self) -> Hash {
+        Hash::hash(self)
+    }
+}
